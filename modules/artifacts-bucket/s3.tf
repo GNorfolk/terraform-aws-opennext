b675901387d36507768e63bac461944d057d0674
George Norfolk <george.norfolk@withintelligence.com>
2026-06-16 10:08:15 +0100

    Linting terraform files.

--- modules/artifacts-bucket/s3.tf
+++ modules/artifacts-bucket/s3.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "artifacts" {
   bucket = var.prefix
-  tags = var.default_tags
+  tags   = var.default_tags
 }
 
 resource "aws_s3_bucket_public_access_block" "artifacts" {
